1e8c0c09a45d5af1d5c0d575db823fb13b3de547
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2022-10-01 08:33:18 -0700

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,18 +1,15 @@
-/**
- * main - Entry point
- * Description: 'the program's description'
- * Return: Always 0 (Success)
- */
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int _putchar(char);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-int _isdigit(int c);
-void puts_half(char *str);
-void print_array(int *a, int n);
-void print_diagonal(int);
-int _putchar(char c);
+void set_string(char **s, char *to);
+
+#endif
