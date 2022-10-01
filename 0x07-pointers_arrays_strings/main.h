6f7ff0da3de0db948c6d499b5ef0a85fbb4dedc7
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2022-10-01 00:59:11 -0700

    0-memeset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+int _isdigit(int c);
+void puts_half(char *str);
+void print_array(int *a, int n);
+void print_diagonal(int);
+int _putchar(char c);
